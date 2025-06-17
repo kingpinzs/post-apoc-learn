7b5b75699280f73c2b70249ccf7f2f45c9db0b51
Jeremy King <kingpinzs@gmail.com>
2025-06-16 19:28:17 -0600

    Add resource management system

---
+++ src/__tests__/resourceSystem.test.js
@@ -0,0 +1,23 @@
+import {
+  allocateResources,
+  freeResources,
+  getUsage,
+  systemCrashed,
+  resetResources,
+} from '../lib/resourceSystem';
+
+beforeEach(() => {
+  resetResources();
+});
+
+test('allocates and frees resources', () => {
+  allocateResources('app1', { cpu: 10, ram: 20, bandwidth: 5 });
+  expect(getUsage()).toEqual({ cpu: 10, ram: 20, bandwidth: 5 });
+  freeResources('app1');
+  expect(getUsage()).toEqual({ cpu: 0, ram: 0, bandwidth: 0 });
+});
+
+test('system crashes when usage reaches limit', () => {
+  allocateResources('app2', { cpu: 100 });
+  expect(systemCrashed()).toBe(true);
+});
