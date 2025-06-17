491c7cee098e66b3abb62207213e76289b90ac28
Jeremy King <kingpinzs@gmail.com>
2025-06-17 11:19:30 -0600

    Add CommandBlock component

--- src/components/scriptbuilder/index.js
+++ src/components/scriptbuilder/index.js
@@ -1,2 +1,3 @@
 export { default as ScriptBuilderScreen } from './ScriptBuilder';
 export { default as ScriptBuilderCanvas } from './ScriptBuilderCanvas';
+export { default as CommandBlock } from './CommandBlock';
