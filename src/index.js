128e49d79bf0ec6b4b62911b5d327ffb71df777b
Jeremy King <kingpinzs@gmail.com>
2025-06-17 23:25:31 -0600

    fix layout height

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AchievementsProvider>
-      <div className="relative w-full min-h-screen overflow-hidden">
+      <div className="relative w-full h-screen overflow-hidden">
         <div className="matrix-bg" />
         <App />
       </div>
