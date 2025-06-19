de82e92ec128459b23191a30aaf13e63afdd5701
Jeremy King <kingpinzs@gmail.com>
2025-06-19 15:34:54 -0600

    Use canStartGame for gating

--- src/__tests__/ApocalypseGame.test.jsx
+++ src/__tests__/ApocalypseGame.test.jsx
@@ -3,22 +3,25 @@ import '@testing-library/jest-dom';
 import ApocalypseGame from '../components/Game';
 
 describe('ApocalypseGame', () => {
-  test('shows boot message then start button', () => {
+  test('shows boot then waits for tutorial', () => {
     jest.useFakeTimers();
     render(<ApocalypseGame />);
     expect(screen.getByText(/INITIATING NEURAL INTERFACE/i)).toBeInTheDocument();
     act(() => {
       jest.advanceTimersByTime(2000);
     });
-    expect(screen.getByText(/INITIATE HACK/i)).toBeInTheDocument();
+    expect(screen.queryByText(/INITIATE HACK/i)).not.toBeInTheDocument();
     jest.useRealTimers();
   });
 
-  test('can start first challenge', () => {
+  test('can start first challenge when ready', () => {
     jest.useFakeTimers();
+    localStorage.setItem('survivos-game-state', JSON.stringify('READY'));
+    const { initGameState } = require('../lib/gameStateManager');
+    initGameState();
     render(<ApocalypseGame />);
     act(() => {
-      jest.advanceTimersByTime(2000);
+      jest.advanceTimersByTime(0);
     });
     fireEvent.click(screen.getByText(/INITIATE HACK/i));
     expect(
